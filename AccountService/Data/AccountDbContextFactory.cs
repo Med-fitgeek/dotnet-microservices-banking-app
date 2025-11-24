1049e699bdf21a59536c40d3e6e0e85e56583b61
Medoune <ndongomedoune.ndiaye@gmail.com>
2025-11-24 20:13:41 +0100

    AuthService: feat: Added Context Factory and first migration

---
+++ AccountService/Data/AccountDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using AccountService.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+public class AccountDbContextFactory : IDesignTimeDbContextFactory<AccountDbContext>
+{
+    public AccountDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AccountDbContext>();
+        optionsBuilder.UseNpgsql("Server=localhost;Database=AccountServiceDB;User ID=postgres;Password=1331;TrustServerCertificate=True;");
+
+        return new AccountDbContext(optionsBuilder.Options);
+    }
+}
---
+++ AccountService/Migrations/20251124191206_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountType = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Balance = table.Column<decimal>(type: "numeric", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
