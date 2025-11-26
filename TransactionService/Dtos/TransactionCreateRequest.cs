fb3aa4c95e23693d35ee982474efa93391214e7b
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2025-11-26 17:36:00 +0100

    TransactionService : feat: Added TransactionCreateRequest dto

--- TransactionService/Dtos/TransactionCreateRequest.cs
+++ TransactionService/Dtos/TransactionCreateRequest.cs
@@ -5,7 +5,7 @@
         public Guid AccountId { get; set; }
         public Guid? TargetAccountId { get; set; }
         public decimal Amount { get; set; }
-        public string Type { get; set; } // CREDIT | DEBIT | TRANSFER
+        public required string Type { get; set; } // CREDIT | DEBIT | TRANSFER
         public string? Description { get; set; }
     }
 }
