51de9dd3e162f6f7e94b2453afe1d4b6e9c4db10
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2025-11-26 17:33:56 +0100

    AccountService : fix: updated the starter and appsettings.json file

--- AccountService/Program.cs
+++ AccountService/Program.cs
@@ -1,19 +1,45 @@
 using AccountService.Data;
+using AccountService.Messaging;
 using AccountService.Services;
 using AccountService.Services.Impl;
-using AuthService.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using RabbitMQ.Client;
 using System.Security.Cryptography;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// ------------------------------
+// Database
+// ------------------------------
 builder.Services.AddDbContext<AccountDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IAccountService, AccountServiceImpl>();
 
+// ------------------------------
+// RabbitMQ
+// ------------------------------
+builder.Services.AddSingleton<IConnection>(sp =>
+{
+    var cfg = sp.GetRequiredService<IConfiguration>();
+    var factory = new ConnectionFactory
+    {
+        HostName = cfg["RabbitMq:Host"],
+        Port = int.Parse(cfg["RabbitMq:Port"]),
+        UserName = cfg["RabbitMq:User"],
+        Password = cfg["RabbitMq:Password"]
+    };
+    return factory.CreateConnection();
+});
+
+// Le consumer va créer les queues/exchanges
+builder.Services.AddHostedService<TransactionConsumer>();
+
+// ------------------------------
+// JWT Auth
+// ------------------------------
 var publicKeyPath = builder.Configuration["Jwt:PublicKeyPath"];
 var publicKeyText = File.ReadAllText(publicKeyPath);
 var rsa = RSA.Create();
@@ -35,14 +61,11 @@ builder.Services
         };
     });
 
-
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
