63bbcf6e1af76102d2aa5819eb7eaabf09596c55
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2025-11-26 17:35:05 +0100

    Created shared project for shared files (dtos, utilities)

---
+++ DineroBank.Shared/DTOs/Transaction/TransactionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DineroBank.Shared.DTOs.Transaction
+{
+    public class TransactionDto
+    {
+        public Guid Id { get; set; }
+        public Guid MessageId { get; set; } = Guid.NewGuid();
+        public Guid AccountId { get; set; }                
+        public Guid? TargetAccountId { get; set; }         
+        public decimal Amount { get; set; }
+        public string Type { get; set; } // CREDIT | DEBIT | TRANSFER
+        // public string Currency { get; set; } = "EUR";
+        public DateTime CreatedAt { get; set; } 
+        public string? Description { get; set; }     
+    }
+}
